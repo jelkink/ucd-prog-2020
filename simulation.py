ebb198a299629d617c7e8c5cff478a063d5dbc28
Jos Elkink <jelkink@gmail.com>
2020-11-20 13:05:27 +0000

    populate simulation class (Nizar and Una)

--- simulation.py
+++ simulation.py
@@ -1,7 +1,24 @@
+from party import Party
+from voter import Voter
+
+#create a simulation class, which creates parties & voters and runs the simulation
+#voters,parties,numbers of simulations
 class Simulation:
 
-  def __init__(self):
-    pass
-  
+  def __init__(self, n_of_simulations):
+    self.n_of_simulations = n_of_simulations
+    self.voters = []
+    self.parties = []
+
+  def generate_parties(self, n):
+    for i in range(n):
+      self.parties.append(Party(self, "Party " + format(i, 'd') , "colour" + format(i, 'd')))
+
+  def get_parties(self):
+    return self.parties
+
+  def get_voters(self):
+    return self.voters
+
   def run(self):
     pass
